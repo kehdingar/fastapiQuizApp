9d7a4dba41566842e53f3992cfef789bc4a80211
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-30 11:20:10 +0100

    feat: include registration route

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from  app.api import auth
+from  app.database.config import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# CORS middleware
+origins = [
+    "http://localhost",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# API routers
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
