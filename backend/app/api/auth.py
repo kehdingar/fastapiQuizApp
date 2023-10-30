9ad4ec0bd697b1452ef90384a43de674a127fc60
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-30 15:50:51 +0100

    fix: add ignore for pycache directories

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv,find_dotenv
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.user import UserCreate,UserCreateResponse
+from app.schemas.user import UserCreate,UserCreateResponse, UserLogin
 from app.api.utils.database import get_db
-from app.api.utils.users import get_user, get_password_hash
+from app.api.utils.users import get_user, get_password_hash,verify_password,create_access_token
+from datetime import timedelta
+import os
+
+
+dotenv_path = find_dotenv(raise_error_if_not_found=True, usecwd=True)
+load_dotenv(dotenv_path)
 
 router = APIRouter()
 
@@ -18,6 +25,17 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@router.post("/login",status_code=status.HTTP_202_ACCEPTED)
+def login(user_data: UserLogin, db: Session = Depends(get_db)):
+    user = get_user(db=db, email=user_data.email)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not Found")
+    if not verify_password(user_data.password,user.password ):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
+    access_token_expires = timedelta(minutes=int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")))
+    access_token = create_access_token(data={"email": user.email,"role":user.role}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "Bearer"}
 
 
 
--- backend/app/api/utils/users.py
+++ backend/app/api/utils/users.py
@@ -1,8 +1,15 @@
+from dotenv import load_dotenv,find_dotenv
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
 from app.models.user import User
 from app.api.utils.database import get_db
+import os
+
+dotenv_path = find_dotenv(raise_error_if_not_found=True, usecwd=True)
+load_dotenv(dotenv_path)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,3 +19,13 @@ def get_password_hash(password):
 def get_user(email: str, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == email).first()
     return user
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, os.environ.get("SECRET_KEY"), algorithm=os.environ.get("ALGORITHM"))
+    return encoded_jwt
