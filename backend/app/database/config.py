7f0fbf15e005f6cea691e9a187cb8adb50d99660
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-30 11:35:23 +0100

    fix: use env variables instead of hard coded value

--- backend/app/database/config.py
+++ backend/app/database/config.py
@@ -1,18 +1,17 @@
+from dotenv import load_dotenv,find_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-# from . config import Settings
+dotenv_path = find_dotenv(raise_error_if_not_found=True, usecwd=True)
+load_dotenv(dotenv_path)
 
 if os.getenv("TESTING"):
     SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
 else:
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@database:5432/quiz_db"
-# SQLALCHEMY_DATABASE_URL = Settings().database_url
+    SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
